6183d19a4c600f5718fbba6ce0745c43be149a35
SAIKIRAN <saikirankotturi979@gmail.com>
2022-08-06 14:19:39 +0530

    Commit

---
+++ HeapSort/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Heaps;
+
+namespace HeapSort
+{
+    public class Sort
+    {
+        
+
+        public void HeapSort(int[] A,int n)
+        {
+            Heaps.Heap heap = new Heaps.Heap();
+
+            for (int i = 0; i < n; i++)
+            {
+                heap.Insert(A[i]);
+
+            }
+
+            int k = n - 1;
+            for (int i = 0; i < n; i++)
+            {
+                A[k] = heap.Delete();
+                k = k - 1;
+            }
+        }
+
+        public void Display(int [] A,int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(A[i] + "");
+
+            }
+            Console.WriteLine();
+        }
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sort sort = new Sort();
+            int[] A = { 63, 54, 74, 65, 90 };
+            Console.WriteLine("Original Array");
+            sort.Display(A,A.Length);
+            sort.HeapSort(A, A.Length);
+            sort.Display(A, A.Length);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Heaps/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heaps
+{
+   public class Heap
+    {
+        public int maxSize;
+        public int[] data;
+        public int cSize;
+
+        public Heap()
+        {
+            maxSize = 10;
+            cSize = 0;
+            data = new int[maxSize];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = -1;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return cSize == 0;
+        }
+
+        public int Length()
+        {
+            return cSize;
+        }
+
+        public void Insert(int e)
+        {
+            if (maxSize==cSize)
+            {
+                Console.WriteLine("Heap has no Space");
+            }
+            cSize = cSize + 1;
+            int hi = cSize;
+
+            while (hi>1 && e > data[hi/2])
+            {
+                data[hi] = data[hi / 2];
+                hi = hi / 2;
+            }
+            data[hi] = e;   
+
+        }
+
+        public int Delete()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Heap is empty");
+                return -1;
+            }
+            int e = data[1];
+            data[1] = data[cSize];
+            data[cSize] = -1;
+            cSize = cSize - 1;
+            int i = 1;
+            int j = i * 2;
+            while (j<=cSize)
+            {
+                if (data[j] < data[j+1])
+                {
+                    j = j + 1;
+                }
+                if (data[i] < data[j])
+                {
+                    int temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                    i = j;
+                    j=i * 2;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return e;
+
+        }
+
+        public int Max()
+        {
+            if(IsEmpty())
+            {
+                Console.WriteLine("Heap is Empty");
+                return -1;
+            }
+            return data[1];
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Heap heap = new Heap();
+            heap.Insert(20);
+            heap.Insert(10);
+            heap.Insert(30);
+            heap.Insert(5);
+            heap.Insert(50); 
+
+
+            heap.Display();
+ 
+            Console.WriteLine(heap.Delete());
+            Console.WriteLine("After deletion");
+            heap.Display();
+            Console.WriteLine(heap.Delete());
+            heap.Display();
+            Console.ReadLine();
+        }
+    }
+}
